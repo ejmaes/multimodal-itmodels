Fix: entropy computation for Colab + dataloaders context computation

utils/entropy_computation.py
@@ -6,7 +6,7 @@ import torch
 from torch.nn.functional import log_softmax
 from tqdm import tqdm
 
-from entropy_computation import GRUModel
+from lstm import GRUModel
 
 LOG_2 = torch.log(torch.tensor(2.))
 
@@ -28,13 +28,13 @@ def sentence_predict(model, tokenizer, text, next_words=20):
     return x, words
 
 
-def test_predict_entropy(lm, dataloader, tokenizer, device, batch_length, max_sent_length):
+def test_predict_entropy(lm, dataloader, tokenizer, device, batch_predict_logits):
     tokens_logp = []
     sent_avg_logp = []
     iterator = tqdm(dataloader, desc='Iteration')
     for _, batch in enumerate(iterator):
         # call function
-        batch_logp, batch_avg_logp = batch_predict_entropy(lm, batch, tokenizer, device, batch_length, max_sent_length)
+        batch_logp, batch_avg_logp = batch_predict_entropy(lm, batch, tokenizer, device, batch_predict_logits)
         tokens_logp.extend(batch_logp)
         sent_avg_logp.extend(batch_avg_logp)
 
@@ -42,32 +42,34 @@ def test_predict_entropy(lm, dataloader, tokenizer, device, batch_length, max_se
     sent_length = [len(x) for x in tokens_logp]
     return sent_avg_logp, tokens_logp, sent_length
 
-def batch_predict_logits(lm, batch):
-    try:#if isinstance(lm,GRUModel): # condition on model
-    #else:
-        with torch.no_grad(): # not using GPU memory
-            outputs = lm(**batch)  # n_sentences, max_sent_len, vocab_size # as a kwargs - inputs + attention_mask
-        # get proba
-        logp_w = log_softmax(outputs.logits, dim=-1)
-    except TypeError: # TODO: do it correctly
-        with torch.no_grad(): # not using GPU memory
-            outputs = lm(batch['input_ids'])  # n_sentences, max_sent_len, vocab_size # as a kwargs - inputs + attention_mask
-        # get proba
-        logp_w = log_softmax(outputs, dim=-1)
+def batch_predict_logits_lm(lm, batch):
+    with torch.no_grad():
+        outputs = lm(**{'input_ids':batch['input_ids'], 'attention_mask':batch['attention_mask']})
+    return outputs.logits
 
-    return logp_w
+def batch_predict_logits_rnn(lm, batch):
+    """Use with non HuggingFace model - otherwise other arguments in the dictionary will generate TypeError, 
+    also return argument is a dict not an object
+    """
+    with torch.no_grad():
+        outputs = lm(batch['input_ids'])
+    return outputs
 
 
 
-def batch_predict_entropy(lm, batch, tokenizer, device, batch_length, max_sent_length): # might add logger here
+def batch_predict_entropy(lm, batch, tokenizer, device, batch_predict_logits): # might add logger here
     """ For one batch, get the entropy of expected words 
 
     Input:
         lm: model
-        dataloader
+        batch: dataloader batch with 'input_ids', 'attention_mask' keys, 'start_idx' (optional)
+        batch_predict_logits: function, which predict function to use 
     """
+    batch_length, max_sent_length = batch['input_ids'].shape
     for k in ['input_ids','attention_mask']:
         batch[k] = batch[k].to(device) # put data on gpu
+    if 'start_idx' not in batch:
+        batch['start_idx'] = [0 for _ in range(batch_length)]
     
     # returns
     batch_avg_logp = []
@@ -75,7 +77,8 @@ def batch_predict_entropy(lm, batch, tokenizer, device, batch_length, max_sent_l
 
     # get predictions
     lm.eval()
-    logp_w = batch_predict_logits(lm, batch)
+    outputs_logits = batch_predict_logits(lm, batch)
+    logp_w = log_softmax(outputs_logits, dim=-1)
     logp_w /= LOG_2 # TODO: check
 
     # for every sentence
@@ -110,7 +113,7 @@ def results_to_df(dataframe:pd.DataFrame, out_file_name:str,
     # could add tokens to make sure which tokens
 
     h_bar = dataframe.groupby('length').agg({"normalised_h": "mean"}).to_dict()['normalised_h']
-    dataframe['xu_h'] = dataframe.apply(lambda x: np.nan if x.length not in h_bar else x.normalized_h/h_bar[x.length], axis=1)
+    dataframe['xu_h'] = dataframe.apply(lambda x: np.nan if x.length not in h_bar else x.normalised_h/h_bar[x.length], axis=1)
 
     dataframe.to_csv(f'{out_file_name}.csv',index=False)
-    return out_file_name
+    return dataframe