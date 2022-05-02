Fix: adaptations on entropy computation

utils/entropy_computation.py
@@ -31,16 +31,18 @@ def sentence_predict(model, tokenizer, text, next_words=20):
 def test_predict_entropy(lm, dataloader, tokenizer, device, batch_predict_logits):
     tokens_logp = []
     sent_avg_logp = []
+    tokens = []
     iterator = tqdm(dataloader, desc='Iteration')
     for _, batch in enumerate(iterator):
         # call function
-        batch_logp, batch_avg_logp = batch_predict_entropy(lm, batch, tokenizer, device, batch_predict_logits)
+        batch_logp, batch_avg_logp, batch_tokens = batch_predict_entropy(lm, batch, tokenizer, device, batch_predict_logits)
         tokens_logp.extend(batch_logp)
         sent_avg_logp.extend(batch_avg_logp)
+        tokens.extend(batch_avg_logp)
 
     iterator.close()
     sent_length = [len(x) for x in tokens_logp]
-    return sent_avg_logp, tokens_logp, sent_length
+    return sent_avg_logp, tokens_logp, sent_length, tokens
 
 def batch_predict_logits_lm(lm, batch):
     with torch.no_grad():
@@ -74,6 +76,7 @@ def batch_predict_entropy(lm, batch, tokenizer, device, batch_predict_logits): #
     # returns
     batch_avg_logp = []
     batch_logp = []
+    batch_tokens = []
 
     # get predictions
     lm.eval()
@@ -85,6 +88,7 @@ def batch_predict_entropy(lm, batch, tokenizer, device, batch_predict_logits): #
     for s_id in range(batch_length): 
         sentence = batch['input_ids'][s_id,:]
         sentence_logp = []
+        sentence_tokens = []
         # for every token
         # XXX: issue with a sentence with no context - 1rst token will not be planned
         for token_index in range(max(0,batch['start_idx'][s_id] - 1), max_sent_length-1): # -1 bc looking ahead
@@ -96,26 +100,35 @@ def batch_predict_entropy(lm, batch, tokenizer, device, batch_predict_logits): #
             # increase non-normalised log probability of the sentence
             token_logp = logp_w[s_id, token_index, w_id].item()
             sentence_logp.append(token_logp)
+            sentence_tokens.append(tokenizer.decode([w_id]))
             #print(f"Sentence {s_id} word idx {token_index} next word id {w_id} is `{tokenizer.decode([w_id])}` - word entropy {token_logp}")
         # append to batch
         batch_logp.append(sentence_logp)
         sentence_logp = np.array(sentence_logp)
         batch_avg_logp.append(- sentence_logp.sum()/sentence_logp.shape[0])
+        batch_tokens.append(sentence_tokens)
     
-    return batch_logp, batch_avg_logp
+    return batch_logp, batch_avg_logp, batch_tokens
 
 
-def results_to_df(dataframe:pd.DataFrame, out_file_name:str,
-                sent_avg_logp:list, tokens_logp:list, sent_length:list):
+def results_to_df(dataframe:pd.DataFrame, sent_avg_logp:list, tokens_logp:list, sent_length:list,
+                        out_file_name:str=None, sentence_tokens:list=None, column_post:str=None):
     # TODO: check all lengths
     dataframe['normalised_h'] = sent_avg_logp
     dataframe['length'] = sent_length
     dataframe['tokens_h'] = tokens_logp
     dataframe['sum_h'] = dataframe.normalised_h * dataframe.length
     # could add tokens to make sure which tokens
+    if sentence_tokens is not None:
+        dataframe['tokens'] = sentence_tokens
 
     h_bar = dataframe.groupby('length').agg({"normalised_h": "mean"}).to_dict()['normalised_h']
     dataframe['xu_h'] = dataframe.apply(lambda x: np.nan if x.length not in h_bar else x.normalised_h/h_bar[x.length], axis=1)
 
-    dataframe.to_csv(f'{out_file_name}.csv',index=False)
+    if column_post is not None:
+        dataframe.rename({col:f'{col}{column_post}' for col in ['normalised_h','length','tokens_h','sum_h','xu_h']}, 
+                            inplace=True)
+
+    if out_file_name is not None:
+        dataframe.to_csv(f'{out_file_name}.csv',index=False)
     return dataframe