77fefd39f61656958f42e0a4203e45478d610f09
Eliot Maës <maesneako@gmail.com>
2022-07-20 19:16:14 +0200

    Add: logging in GPT + more params

--- experiments/gpt_train.py
+++ experiments/gpt_train.py
@@ -18,6 +18,7 @@
 
 """
 
+from lib2to3.pgen2 import token
 import numpy as np
 import os, sys
 import pandas as pd
@@ -29,25 +30,32 @@ from datetime import datetime
 import argparse
 from types import SimpleNamespace
 import logging
+import random
 
 #### Sklearn
 from sklearn.model_selection import train_test_split
 
 #### Pytorch, HuggingFace
-need_install = []
 try:
+    import torch
+except ModuleNotFoundError:
+    import pip
+    pip.main(['install', 'transformers'])
     import torch
     import torch.nn.functional as F
     import torch.optim as optim
-except:
-    need_install.append('pytorch')
 try:
+    from transformers import AutoTokenizer
+    #from datasets import load_dataset
+except:
+    import pip
+    pip.main(['install', 'transformers'])
+    #pip.main(['install', 'datasets'])
     from transformers import AutoTokenizer
     # Functions for saving during training already exist
     from transformers import Trainer, TrainingArguments, AutoModelForCausalLM
     from transformers import TextDataset, DataCollatorForLanguageModeling
-except:
-    need_install.append('transformers')
+
 
 #### From github
 UTILS_PATH = "../utils"
@@ -64,7 +72,12 @@ from entropy_computation import pivot_results_df
 DATA_PATH = "../data"
 SAVE_PATH = '../models'
 DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
+
 SEED = 42
+torch.manual_seed(SEED)
+torch.cuda.manual_seed(SEED)
+np.random.seed(SEED)
+random.seed(SEED)
 
 with open("accounts_params.json", "r") as f:
     f = json.load(f)
@@ -73,9 +86,13 @@ HUB_PARAMS = f['huggingface'] # keys: user, token
 #%% ------ Arguments
 def parse_arguments():
     parser = argparse.ArgumentParser()
+    # Model
     parser.add_argument("--model", type=str, required=True, help="Which model to load from HuggingFace.")
+    # Tokenizer
     parser.add_argument("--tokenizer", "-t", type=str, default=None, help="Name of the tokenizer to load from HuggingFace, if different from model name.") 
     # TODO: add mode to train tokenizer from BPE
+    parser.add_argument("--tok_maxlength", "-tml", type=int, default=1024, help="Max length (padding) for tokenizer to create examples.") 
+    # Model / Data Parameters
     parser.add_argument("--conf_file", type=str, required=True, help="Path to the file containing training arguments & data info")
     parser.add_argument("--dataloader", "-d", type=str, default="gpt2", choices=["auto","custom"], help="Whether to use a custom DataLoader or the existing one for training.") 
     
@@ -90,14 +107,12 @@ def parse_arguments():
         args.column_option = training_config['Dataset']['column_option']
         args.traintest = None if 'TrainTest' not in training_config['Dataset'].keys() else training_config['Dataset']['TrainTest']
         args.training_kwargs = training_config['Trainer']
+        args.dataloader_kwargs = training_config["DataLoader"]
 
     return args
 
 #%% ------ LM Train
 if __name__ == '__main__':
-    if len(need_install) > 0:
-        raise ModuleNotFoundError(f"Missing packages: {', '.join(need_install)}")
-
     args = parse_arguments()
     # default arguments
     # args = SimpleNamespace(model_type="gru", data_path="data/corlec", batch_size=8, nb_epochs=10, tokenizer="gpt2")
@@ -107,10 +122,12 @@ if __name__ == '__main__':
 
     ###### Tokenizer
     tokenizer = AutoTokenizer.from_pretrained(args.tokenizer)
+    tok_cont_kwargs = {'truncation':True, 'padding':'max_length', 'max_length':args.tok_maxlength}
     # Adding tokens for padding
     tokenizer.pad_token = tokenizer.eos_token
     tokenizer.sep_token = tokenizer.eos_token
 
+    logging.info('Loading data...')
     ###### Training (?) data
     data_collator = DataCollatorForLanguageModeling(tokenizer = tokenizer, mlm=False)
     df = pd.read_csv(args.data, keep_default_na=False, na_values=['']) # na values: 'nan' for french
@@ -125,8 +142,13 @@ if __name__ == '__main__':
             #if len(args.column_option["group_columns"]) == 1:
             files = df[args.file_column].unique()
             files_train, files_test = train_test_split(files, random_state=SEED, test_size=0.3)
+    logging.info(f'Training files: \n{files_train} \n\n Testing files: \n{files_test}')
 
-    dataset_c, df2 = create_context_dataset(df, tokenizer, files_train, files_test, sep_token=tokenizer.eos_token, max_length=150)
+    sep = {"space": " ", "eos": tokenizer.eos_token}
+    sep = sep["eos"] if "separator" not in args.dataloader_kwargs.keys() else sep[[args.dataloader_kwargs["separator"]]]
+    max_length = 150 if "context_max_length" not in args.dataloader_kwargs.keys() else args.dataloader_kwargs["context_max_length"]
+
+    dataset_c, df2 = create_context_dataset(df, tokenizer, files_train, files_test, sep_token=sep, max_length=max_length, **args.column_option)
 
     ###### Model and Training
     model = AutoModelForCausalLM.from_pretrained(args.model).to(DEVICE)
@@ -152,7 +174,10 @@ if __name__ == '__main__':
         train_dataset = dataset_c['train_dataset'],
         eval_dataset = dataset_c['test_dataset']
     )
+    
+    logging.info(f"Training model")
     trainer.train()
     trainer.save_model() # model will be saved in the training folder
     # lm = AutoModelForCausalLM.from_pretrained(output_dir, local_files_only=True) # loading model
     trainer.push_to_hub(MODEL_NAME) 
+    logging.info(f"Model saved to Hub! Total time elapsed: {datetime.now() - start}")
